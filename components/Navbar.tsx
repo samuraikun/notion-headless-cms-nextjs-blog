7d0d7e757b7cd646a10fba38c3fc44a7ad242afa
samuraikun <tazitaziawawa@gmail.com>
2023-01-17 16:21:24 +0900

    add site config

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { siteConfig } from '@/site.config'
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,7 +8,7 @@ export default function Navbar() {
       <div className='container-fluid w-full flex flex-wrap items-center justify-between px-6'>
         <div className='bg-grey-light rounded-t-md w-full' aria-label='breadcrumb'>
           <Link href='/' className='text-gray-500 hover:text-gray-600'>
-            Samuraikun Blog
+            {siteConfig.title}
           </Link>
           {/* Breadcrumb */}
         </div>
---
+++ site.config.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: 'Samuraikun Blog',
+  twitterUrl: 'https://twitter.com/YuxBeta',
+  githubUrl: 'https://github.com/samuraikun',
+}
