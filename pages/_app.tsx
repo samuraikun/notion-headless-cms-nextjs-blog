d69fc7e5b933a85e84a726e4a0184cf3feb20f0e
samuraikun <tazitaziawawa@gmail.com>
2023-01-18 20:21:04 +0900

    Applying dark mode to the entire application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import Loader from '@/components/Loader'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider attribute='class'>
       <Loader />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
