5e180bfbeca52c9dce2de8ea00d1e5e2ea763968
samuraikun <tazitaziawawa@gmail.com>
2023-01-18 15:36:42 +0900

    パンくずリストの追加

---
+++ components/Breadcrumb.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const Breadcrumb = () => {
+  const router = useRouter()
+  let joinedPath = ''
+  return (
+    <>
+      {router.asPath.split('/').map((path, index) => {
+        if (path) {
+          joinedPath += path + '/'
+          return (
+            <Link  key={index} href={`/${joinedPath}`} className='text-gray-500 hover:text-gray-600'>
+              <span className='text-gray-500 mx-2'>/</span> {path}
+            </Link>
+          )
+        }
+      })}
+    </>
+  )
+}
+
+export default Breadcrumb
